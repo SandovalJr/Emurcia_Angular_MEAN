1e8c78cbf17ea4c17018c620581fa9f2d51218ce
Eduardo Sandoval <edubizzle7@gmail.com>
2020-07-10 10:22:30 -0500

    boton de regresar al sistema jalando

--- src/app/components/pw/partials/navbar/navbar.component.ts
+++ src/app/components/pw/partials/navbar/navbar.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from "../../../../../services/authentication.service";
+import { Component, OnInit } from "@angular/core";
+import {
+  AuthenticationService,
+  TokenPayload,
+} from "../../../../../services/authentication.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
+  constructor(public auth: AuthenticationService, private router: Router) {}
+  credentials: TokenPayload = {
+    id: 0,
+    usuario: "",
+    password: "",
+    user_type: "",
+    region: "",
+  };
+  ngOnInit(): void {}
 
-  constructor(public auth: AuthenticationService) { }
-
-  ngOnInit(): void {
+  IrSistema() {
+    if (this.auth.IsSupervisor() === 2) {
+      this.router.navigateByUrl("/SupervisorProfile/Inicio_Supervisor");
+    } else if (this.auth.IsVendedor() === 3) {
+      this.router.navigateByUrl("/VendedorProfile/Inicio_Vendedor");
+    } else {
+      this.router.navigateByUrl("/loginError");
+    }
   }
-
 }
