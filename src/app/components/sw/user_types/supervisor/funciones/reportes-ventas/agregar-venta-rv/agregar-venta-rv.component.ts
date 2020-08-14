67c1c1db486d6f29eb4be96691e96f454745bacd
Eduardo Sandoval <edubizzle7@gmail.com>
2020-08-13 21:07:49 -0500

    se creo el service

--- src/app/components/sw/user_types/supervisor/funciones/reportes-ventas/agregar-venta-rv/agregar-venta-rv.component.ts
+++ src/app/components/sw/user_types/supervisor/funciones/reportes-ventas/agregar-venta-rv/agregar-venta-rv.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { FormControl, FormGroup } from "@angular/forms";
+import {
+  RxwebValidators,
+  ReactiveFormConfig,
+  NumericValueType,
+} from "@rxweb/reactive-form-validators";
+import { MessageErrorsService } from "../../../../../../../../services/messageError.service";
 @Component({
-  selector: 'app-agregar-venta-rv',
-  templateUrl: './agregar-venta-rv.component.html',
-  styleUrls: ['./agregar-venta-rv.component.scss']
+  selector: "app-agregar-venta-rv",
+  templateUrl: "./agregar-venta-rv.component.html",
+  styleUrls: ["./agregar-venta-rv.component.scss"],
 })
 export class AgregarVentaRVComponent implements OnInit {
+  public formulario: FormGroup;
+  // credentialsRV: AutosLoad = {
+  //   id_auto: 0,
+  //   Num_eco: null,
+  //   anio: null,
+  //   ubicacion: "",
+  //   num_serie: "",
+  //   marca_auto: "",
+  //   modelo: "",
+  //   placas: "",
+  //   chofer_ruta: "",
+  //   cilindros_piezas: null,
+  //   marca: "",
+  // };
 
-  constructor() { }
+  constructor(
+    private activatedRouter: ActivatedRoute,
+    private router: Router,
+    private MessageErrorSvr: MessageErrorsService
+  ) {}
 
   ngOnInit(): void {
+    this.creatForm();
+
+    // console.log(` la fecha de parametro es:ARV ${createdAt}`);
+    // console.log(` la marca de parametro es:ARV ${marca} y la region es ${region}`);
   }
 
+  public creatForm() {
+    this.formulario = new FormGroup({
+      Num_eco: new FormControl(null, [
+        RxwebValidators.required(),
+        RxwebValidators.numeric(),
+      ]),
+    });
+  }
+
+  public ValidarFormulario(control: string) {
+    if (!this.formulario.controls[control].touched) return { error: undefined };
+    return this.MessageErrorSvr.errorMessage(
+      this.formulario.controls[control].errors
+    );
+  }
+
+  AgregarVenta() {
+    const marca = this.activatedRouter.snapshot.paramMap.get("marca");
+    const region = this.activatedRouter.snapshot.paramMap.get("region");
+    const createdAt = this.activatedRouter.snapshot.paramMap.get("createdAt");
+  }
 }
---
+++ src/services/reporteDeVentas.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
+
+export interface ReporteVentasDetails {
+  id_RV: number;
+  marca: string;
+  region: string;
+  auto: string;
+  Cilidros_Vacios: number;
+  Venta_Kilos: number;
+  Precio_Prom: number;
+  Importe_Liquidar: number;
+  Recibe_caja_efectivo: number;
+  credito_cilindro: number;
+  Importe_credito: number;
+  Cel_Tel_Cliente: string;
+  Litros_Consumo: number;
+  INV_CIL: number;
+  Equiv_KG: number;
+  Comentarios: string;
+  exp: number;
+  iat: number;
+}
+
+export interface ReporteVentasLoad {
+  id_RV: number;
+  marca: string;
+  region: string;
+  auto: string;
+  Venta_Kilos: number;
+  Precio_Prom: number;
+  Importe_Liquidar: number;
+  Recibe_caja_efectivo: number;
+  credito_cilindro: number;
+  Importe_credito: number;
+  Cel_Tel_Cliente: string;
+  Litros_Consumo: number;
+  INV_CIL: number;
+  Equiv_KG: number;
+  Comentarios: string;
+  Cilidros_Vacios: number;
+}
+
+@Injectable()
+export class autosService {
+  baseUrl = "http://localhost:3000/api/Reportes_Ventas/";
+}
